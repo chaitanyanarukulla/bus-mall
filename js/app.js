26b3fed71489ed208f13a6f161be8b27ca9ded3c
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-04-08 16:03:52 -0700

    Updated app.js check images and keep count

---
+++ js/app.js
@@ -0,0 +1,88 @@
+'use strict';
+var allProducts = [];
+var newrender = [];
+var oldrender = [];
+var totalClicks = 0;
+var productPic = document.getElementById('imgrool');
+
+new Product('bag', 'img/bag.jpg');
+new Product('banana', 'img/banana.jpg');
+new Product('bathroom', 'img/bathroom.jpg');
+new Product('boots', 'img/boots.jpg');
+new Product('breakfast', 'img/breakfast.jpg');
+new Product('bubblegum', 'img/bubblegum.jpg');
+new Product('chair', 'img/chair.jpg');
+new Product('cthulhu', 'img/cthulhu.jpg');
+new Product('dog', 'img/dog-duck.jpg');
+new Product('dragon', 'img/dragon.jpg');
+new Product('pen', 'img/pen.jpg');
+new Product('pet', 'img/pet-sweep.jpg');
+new Product('scissors', 'img/scissors.jpg');
+new Product('shark', 'img/shark.jpg');
+new Product('sweep', 'img/sweep.png');
+new Product('tauntaum', 'img/tauntaun.jpg');
+new Product('unicorn', 'img/unicorn.jpg');
+new Product('usb', 'img/usb.jpg');
+new Product('water', 'img/water-can.jpg');
+new Product('wine', 'img/wine-glass.jpg');
+
+// Constructor Function to make Product  --------------------------------------------------->
+function Product(name, path) {
+  this.path = path;
+  this.name = name;
+  this.views = 0;
+  this.clicks = 0;
+  allProducts.push(this);
+}
+//function for  3 random picture which are not repeted ---------------------------------------------------------------->
+function randomPic() {
+  newrender=[];
+  while (newrender.length < 3) {
+    var randomNum = Math.floor(Math.random() * allProducts.length);
+    console.log('this is random #',randomNum);
+    if (!newrender.includes(allProducts[randomNum]) && !oldrender.includes(allProducts[randomNum])) {
+      newrender.push(allProducts[randomNum]);
+    }
+    else{
+      randomPic();
+    }
+  }
+  oldrender = newrender;
+}
+// putting img on screen-------------------------------------------------------------------------->
+function render() {
+  randomPic();
+  for (var i = 0; i < newrender.length; i++) {
+    var imgEl = document.createElement('img');
+    imgEl.src = newrender[i].path;
+    imgEl.id = newrender[i].name;
+    productPic.appendChild(imgEl);
+    newrender[i].views++;
+  }
+}
+// Wipe the window imgs------------------------------------------------------->
+function wipe() {
+  while (productPic.firstChild) {
+    productPic.removeChild(productPic.firstChild);
+  }
+}
+
+// Event handler--------------------------------------------------------------->
+function handleClick(event) {
+  for (var i = 0; i < newrender.length; i++) {
+    if (event.target.id === newrender[i].name) {
+      newrender[i].clicks++;
+      totalClicks++;
+    }
+  }
+  if (totalClicks === 25) {
+    productPic.removeEventListener('click', handleClick);
+    wipe();
+  } else {
+    wipe();
+    render();
+  }
+}
+render();
+productPic.addEventListener('click', handleClick);
+
---
+++ user_stories.md
@@ -0,0 +1,5 @@
+#Marketing research team Building project to get User data to research what products are liked then others. user will be given 20 total images to select from and each will be given 25 tries
+
+#focus group The browser will show 3 picture and out of those 3 they can only select one pictures are generated randomly no 3 picture are repeated This process will happen 25 times
+
+#Deloper problems to solve randomly generate 3 images to show up on browser collect data from users on what they click and how many times they click
